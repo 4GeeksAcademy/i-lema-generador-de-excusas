a6c48c59485b91437231d3d80f403f20b280ac0c
i-lema <161314957+i-lema@users.noreply.github.com>
2024-03-25 17:33:52 +0000

    Cambios en js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,70 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generarExcusa(quien, accion, que, cuando) {
+  let quienAleatorio = Math.floor(Math.random() * quien.length);
+  let accionAleatoria = Math.floor(Math.random() * accion.length);
+  let queAleatorio = Math.floor(Math.random() * que.length);
+  let cuandoAleatorio = Math.floor(Math.random() * cuando.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let excusa =
+    quien[quienAleatorio] +
+    " " +
+    accion[accionAleatoria] +
+    " " +
+    que[queAleatorio] +
+    " " +
+    cuando[cuandoAleatorio];
+  return excusa;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = [
+    "Mi perro",
+    "Un fantasma",
+    "Un encapuchado",
+    "Un duende",
+    "Mi gato",
+    "Un dragón",
+    "Un alienígena",
+    "Mi vecino",
+    "Un mago",
+    "Mi hermana"
+  ];
+  let accion = [
+    "se comió",
+    "robó",
+    "destruyó",
+    "quemó",
+    "rompió",
+    "vendió",
+    "escondió",
+    "desintegró",
+    "hizo desaparecer",
+    "tiró al mar"
+  ];
+  let que = [
+    "mi proyecto",
+    "mi ordenador",
+    "mi teléfono",
+    "mi coche",
+    "mis deberes",
+    "mi teclado",
+    "mi libro",
+    "mi código",
+    "mi portátil",
+    "mi disco duro"
+  ];
+  let cuando = [
+    "cuando estaba en la ducha",
+    "mientras dormía",
+    "mientras hacía la cama",
+    "cuando estaba comiendo",
+    "mientras iba a clase",
+    "cuando estaba de camino",
+    "cuando estaba a punto de salir",
+    "mientras trabajaba",
+    "mientras no miraba",
+    "cuando fui a la compra"
+  ];
+
+  let excusaAleatoria = generarExcusa(quien, accion, que, cuando);
+  document.getElementById("excuse").innerHTML = excusaAleatoria;
 };
